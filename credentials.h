079641d8013970cead04a8fa69dd818a1b7229b5
G6EJD <d.l.bird@sky.com>
2021-04-15 18:36:56 +0100

    Add files via upload

---
+++ credentials.h
@@ -0,0 +1,14 @@
+// Comment out for NO access controls
+#define AccessControl
+
+// Set your server access username and password here (OPTIONAL:)
+const char* http_username = "admin";
+const char* http_password = "admin";
+
+// Set a server logical name, this gets translated to the physical IP address e.g. http://192.168.0.22
+// NOTE: ONLY works if your browser supports this function
+const char* ServerName    = "fileserver";
+
+// Set your Wi-Fi Credentials here
+const char* ssid          = "yourSSID";
+const char* password      = "yourPASSWORD";
